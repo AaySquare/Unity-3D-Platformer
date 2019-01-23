e643fb1ac27009202d82b694c9eb92d57bd645c8
Aayush Mathur <aayushmathur14@gmail.com>
2019-01-23 22:32:02 +0000

    Initial player controller

---
+++ Untitled 3D Platformer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 offset;
+
+    public float rotateSpeed;
+
+    public Transform pivot;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate ()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(-vertical, 0, 0);
+
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(0, desiredYAngle, 0);
+        transform.position = target.position + (rotation * offset);
+
+        transform.LookAt(target.position);
+    }
+}
---
+++ Untitled 3D Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 10.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 2.5f;
+
+    private Vector3 movement;
+    private bool doubleJump;
+    public CharacterController characterController;
+
+    // Use this for initialization
+    void Start ()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yMove = movement.y;
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        movement = (transform.right * moveHorizontal) + (transform.forward * moveVertical);
+        movement = movement.normalized * moveSpeed;
+        movement.y = yMove;
+
+        if (characterController.isGrounded)
+        {
+            movement.y = 0f;
+            doubleJump = true;  
+        }
+            
+        if (characterController.isGrounded || doubleJump)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                if (!characterController.isGrounded)
+                    doubleJump = false;
+
+                movement.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity
+        movement.y = movement.y + (Physics.gravity.y * gravity * Time.deltaTime);
+
+        // Move the controller
+        characterController.Move(movement * Time.deltaTime);
+
+    }
+}
